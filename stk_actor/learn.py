5bc15748c9517622ab1e5e15892b8308f4abcf7b
smilhey <smail.elhafdi@gmail.com>
2025-02-16 18:53:41 +0100

    training loop for test

--- stk_actor/learn.py
+++ stk_actor/learn.py
@@ -1,3 +1,6 @@
+from tqdm import tqdm
+import gymnasium as gym
+import numpy as np
 from pathlib import Path
 from pystk2_gymnasium import AgentSpec
 from functools import partial
@@ -7,33 +10,83 @@ from bbrl.agents.gymnasium import ParallelGymAgent, make_env
 from bbrl.agents import Agents, TemporalAgent
 from bbrl.workspace import Workspace
 
+from .my_wrappers import (
+    ObsFilterWrapper,
+    RewardCenterTrackWrapper,
+    FlattenObservationWrapper,
+    ActionFilterWrapper,
+    ActionTrackerWrapper,
+    StuckResetWrapper,
+    RewardWrapper,
+    RewardSmoothWrapper,
+)
+
 
 # Note the use of relative imports
-from actors import Actor, SamplingActor
-from pystk_actor import env_name, get_wrappers, player_name
+from .actors import ActorSac
+from .pystk_actor import env_name, get_wrappers, player_name
+
+
+n_steps = 300
+n_epochs = 1
+scale_distance_center = 0.05
+rewards = np.zeros((n_epochs, n_steps))
+obs_filters = [
+    "distance_down_track",
+    "front",
+    "max_steer_angle",
+    "center_path",
+    "center_path_distance",
+    "paths_start",
+    "paths_end",
+    "paths_width",
+    "paths_distance",
+    "velocity",
+    "acceleration",
+    "steer",
+    "sliding_velocity",
+    "items_position",
+    "items_type",
+]
+action_filters = ["acceleration", "steer", "drift", "nitro"]
 
 
 if __name__ == "__main__":
     # Setup the environment
-    make_stkenv = partial(
-        make_env,
-        env_name,
-        wrappers=get_wrappers(),
-        render_mode=None,
-        autoreset=True,
-        agent=AgentSpec(use_ai=False, name=player_name),
+    env = gym.make(
+        env_name, render_mode="human", agent=AgentSpec(use_ai=False, name=player_name)
     )
+    wrappers = get_wrappers()
 
-    env_agent = ParallelGymAgent(make_stkenv, 1)
-    env = env_agent.envs[0]
+    env = ActionFilterWrapper(env, action_filters)
+    env = ObsFilterWrapper(env, obs_filters, action_filters)
+    env = StuckResetWrapper(env, speed_threshold=0.5, stuck_time=1)
+    env = ActionTrackerWrapper(env, action_filters)
+    env = RewardCenterTrackWrapper(env, scale_distance_center)
+    env = RewardWrapper(env)
+    env = RewardSmoothWrapper(env)
+
+    actor = ActorSac(obs_space=env.observation_space, action_space=env.action_space)
+    agent = actor.agent
 
     # (2) Learn
 
-    actor = SamplingActor(env.action_space)
-    temporal_agent = TemporalAgent(Agents(env_agent, actor))
+    for e in tqdm(range(n_epochs)):
+        obs = env.reset()[0]
+        episode_reward = 0  # Track total episode reward
+        for t in range(n_steps):
+            action = agent.choose_action(obs)
+            new_obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            agent.remember(obs, action, reward, new_obs, done)
 
-    workspace = Workspace()
-    temporal_agent(workspace, t=0, n_steps=10)
+            # Perform a learning step and get loss values
+            loss_dict = agent.learn()
+            obs = new_obs
+            if done:
+                obs = env.reset()[0]  # Reset and start next episode
+                break
+    env.close()
 
     # (3) Save the actor sate
     mod_path = Path(inspect.getfile(get_wrappers)).parent
--- stk_actor/pystk_actor.py
+++ stk_actor/pystk_actor.py
@@ -2,34 +2,59 @@ from typing import List, Callable
 from bbrl.agents import Agents, Agent
 import gymnasium as gym
 
+from .my_wrappers import (
+    ObsFilterWrapper,
+    RewardCenterTrackWrapper,
+    FlattenObservationWrapper,
+    ActionFilterWrapper,
+    ActionTrackerWrapper,
+    StuckResetWrapper,
+    RewardWrapper,
+    RewardSmoothWrapper,
+)
+
 # Imports our Actor class
 # IMPORTANT: note the relative import
-from actors import Actor, MyWrapper, ArgmaxActor, SamplingActor
+from .actors import ActorSac
 
 #: The base environment name
-env_name = "supertuxkart/flattened_multidiscrete-v0"
+env_name = "supertuxkart/simple-v0"
 
 #: Player name
-player_name = "Example"
+player_name = "smail_gaetan_kart"
 
 
 def get_actor(
     state, observation_space: gym.spaces.Space, action_space: gym.spaces.Space
 ) -> Agent:
-    actor = Actor(observation_space, action_space)
-
-    # Returns a dummy actor
-    if state is None:
-        return SamplingActor(action_space)
-
+    actor = ActorSac(observation_space, action_space)
     actor.load_state_dict(state)
-    return Agents(actor, ArgmaxActor())
+    return Agents(actor)
 
 
 def get_wrappers() -> List[Callable[[gym.Env], gym.Wrapper]]:
     """Returns a list of additional wrappers to be applied to the base
     environment"""
+    obs_filters = [
+        "distance_down_track",
+        "front",
+        "max_steer_angle",
+        "center_path",
+        "center_path_distance",
+        "paths_start",
+        "paths_end",
+        "paths_width",
+        "paths_distance",
+        "velocity",
+        "acceleration",
+        "steer",
+        "sliding_velocity",
+        "items_position",
+        "items_type",
+    ]
+    action_filters = ["acceleration", "steer", "drift", "nitro"]
+    scale_distance_center = 0.05
     return [
-        # Example of a custom wrapper
-        lambda env: MyWrapper(env, option="1")
+        lambda env: ActionFilterWrapper(env, action_filters),
+        lambda env: ObsFilterWrapper(env, obs_filters, action_filters),
     ]
